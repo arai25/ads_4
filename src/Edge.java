efadca6bdff5611c12075ec3fd1a1a7e85050ef2
arai25 <araibidanova@mail.ru>
2024-05-24 20:18:29 +0500

    Added Edge class to represent graph edges

---
+++ src/Edge.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Edge<Vertex> {
+    private Vertex source;
+    private Vertex dest;
+    private Double weight;
+
+    public Edge(Vertex source, Vertex dest, Double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    //getters & setters
+}
+
+
